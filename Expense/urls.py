1fddc5231c1d9d522074b3bcb2ed6d76cdfd4a8e
skyhill <skyhill4321@gmail.com>
2025-04-05 12:20:52 +0545

    Completed CRUD features for Expense Tracker

--- Expense/urls.py
+++ Expense/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from Expense import views
 
+app_name = 'Expense'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',views.Home, name='ExpenseView'),
     path('add/',views.ExpenseAddView, name='ExpenseAddView'),
-    path('delete/',views.ExpenseDeleteView, name='ExpenseDeleteView'),
-    path('update/',views.ExpenseUpdateView, name='ExpenseUpdateView'),
-    path('view/',views.ExpenseViewView, name='ExpenseView'),
+    path('delete/<int:id>/',views.ExpenseDeleteView, name='ExpenseDeleteView'),
+    path('edit/<int:id>/',views.ExpenseUpdateView, name='ExpenseUpdateView'),
+    path('view/',views.ExpenseViewView, name='ExpenseViewView'),
 ]
--- Expense/views.py
+++ Expense/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from django.forms import forms
 from .forms import ExpenseForm
@@ -11,24 +11,42 @@ def Home(request):
 
 
 def ExpenseAddView(request):
+    success_message = "None"
     if request.method == 'POST':
         form = ExpenseForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponse("Expense added successfully")
+            success_message = "Expense added successfully!"
     else:
         form = ExpenseForm()
 
-    return render(request, 'add.html', {'form': form})
+    return render(request, 'add.html', {'form': form, 'success_message': success_message})
     
 
-def ExpenseDeleteView(request):
-    return HttpResponse("This is the delete view with id ")
+def ExpenseDeleteView(request, id):
+    expense = get_object_or_404(Expense, pk=id)
+    if request.method == 'POST':
+        expense.delete()
+        return redirect('Expense:ExpenseViewView')  
+    return render(request, 'delete.html', {'expense': expense})
 
-def ExpenseUpdateView(request):
-    return HttpResponse("This is the update view with id ")
+def ExpenseUpdateView(request, id):
+    expense = get_object_or_404(Expense, pk=id)
 
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('Expense:ExpenseViewView')  # Make sure this URL name exists
+    else:
+        form = ExpenseForm(instance=expense)
+
+    return render(request, 'update.html', {'form': form, 'expense': expense})
+
+     
+    
 def ExpenseViewView(request):
     expense_view = Expense.objects.all()
-    return render(request, 'view.html', {'expenses': expense_view})
+    return render(request, 'view.html', {'expense_view': expense_view})
+
 
